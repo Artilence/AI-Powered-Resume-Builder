4c65e5a2232d3b21ce3a88a935c7bd17061782ae
sohaib anwaar <sohaibanwaar36@gmail.com>
2025-01-22 14:33:37 +0500

    changing the varaibles versions

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,28 +1,28 @@
-import arrowDown from './arrowDown.png';
-import T_Undo from './t_undo.png';
-import T_Redo from './t_redo.png';
-import T_Text from './t_text.png';
-import T_alignleft from './t_alignleft.png';
-import T_Bold from './t_bold.png';
-import T_Italic from './t_italic.png';
-import T_Underline from './t_underline.png';
-import T_Strikethrough from './t_strikethrough.png';
-import T_inlineCode from './t_inlinecode.png';
-import T_BulletList from './t_bulletpoints.png';
-import T_NumberedList from './t_listpoints.png';
-import logo from './Logo.png';
-import linkedin from './linkedinIcon.png';
-import google from './googleIcon.png';
+import arrowDown from "./arrowDown.png";
+import T_Undo from "./t_undo.png";
+import T_Redo from "./t_redo.png";
+import T_Text from "./t_text.png";
+import T_alignleft from "./t_alignleft.png";
+import T_Bold from "./t_bold.png";
+import T_Italic from "./t_italic.png";
+import T_Underline from "./t_underline.png";
+import T_Strikethrough from "./t_strikethrough.png";
+import T_inlineCode from "./t_inlinecode.png";
+import T_BulletList from "./t_bulletpoints.png";
+import T_NumberedList from "./t_listpoints.png";
+import logo from "./Logo.png";
+import linkedin from "./linkedinIcon.png";
+import google from "./googleIcon.png";
 
-import arrowLeft from './arrowLeft.png';
-import deleteDocument from './delete_icon.png';
-import documentIcon from './document_icon.png';
-import downloadIcon from './download_icon.png';
-import duplicateDocument from './duplicate_icon.png';
-import menu1 from './menu1.png';
-import openDocument from './openDocument.png';
-import myDocuments from './myDocuments.png';
-import dummyprofile from './dummyprofile.png';
+import arrowLeft from "./arrowLeft.png";
+import deleteDocument from "./delete_icon.png";
+import documentIcon from "./document_icon.png";
+import downloadIcon from "./download_icon.png";
+import duplicateDocument from "./duplicate_icon.png";
+import menu1 from "./menu1.png";
+import openDocument from "./openDocument.png";
+import myDocuments from "./mydocuments.png";
+import dummyprofile from "./dummyprofile.png";
 
 export {
   myDocuments,
@@ -41,7 +41,6 @@ export {
   logo,
   linkedin,
   google,
-
   arrowLeft,
   deleteDocument,
   documentIcon,
@@ -50,5 +49,4 @@ export {
   menu1,
   openDocument,
   dummyprofile,
-
 };
